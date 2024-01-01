0f967d2c28d1d457968875ee34d8eea932b87c87
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2024-01-01 19:55:04 +1000

    Create 03natgw.tf
    Add NAT GW code

---
+++ 03natgw.tf
@@ -0,0 +1,26 @@
+# Add NAT Gateway
+resource "azurerm_nat_gateway" "natgw" {
+  name                    = "nat-Gateway"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+}
+
+resource "azurerm_subnet_nat_gateway_association" "natgwassoc" {
+  subnet_id      = azurerm_subnet.snet.id
+  nat_gateway_id = azurerm_nat_gateway.natgw.id
+}
+
+resource "azurerm_public_ip" "gwpip" {
+  name                = "gwPIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "pipassoc" {
+  nat_gateway_id       = azurerm_nat_gateway.natgw.id
+  public_ip_address_id = azurerm_public_ip.gwpip.id
+}
