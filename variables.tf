d5a133fca8cda9e625a78f74239ed12d6352f97f
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2024-01-23 20:25:01 +1000

    Add Azure Bastion

--- variables.tf
+++ variables.tf
@@ -7,8 +7,8 @@ variable "admin_password" {
 # Define our IP address or range to use in NSG and Azure firewall for SSH access
 variable "where_we_are_at" {
   description = "IP address we are working from to use in NSG and firewall for SSH access."
-  type = string
-  default = "14.202.22.1/32"
+  type        = string
+  default     = "14.202.22.1/32"
 }
 
 # 01a-vm.tf 
@@ -26,14 +26,14 @@ variable "Location" {
 
 variable "vnet_ip" {
   description = "IP address range for virtual network vnet1"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "snet1_ip" {
   description = "Network address for snet1"
-  type = string
-  default = "10.0.1.0/24"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 # Leave snet2's IP range hard coded as second VM and VNet will not always be deployed.
